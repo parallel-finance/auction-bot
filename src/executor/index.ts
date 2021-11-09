13f099221c58c3f1cc9d733dff3cbae139410486
Alan WANG <alan@parallel.fi>
2021-11-09 16:13:04 +0800

    Dispatch task with different paraId

---
+++ src/executor/index.ts
@@ -0,0 +1,21 @@
+import { ApiPromise } from "@polkadot/api";
+import type { ContributionTask } from "../query";
+
+const defaultExecutor = async (api: ApiPromise, task: ContributionTask) => {
+  return api.tx.utility.batchAll([
+    api.tx.system.remark(task.id),
+    api.tx.proxy.proxy(
+      process.env.PROXIED_ACCOUNT as string,
+      null,
+      api.tx.crowdloan.contribute(task.paraId, task.amount, null)
+    ),
+  ]);
+};
+
+export const WHITELIST = {
+  2006: defaultExecutor,
+  2012: defaultExecutor,
+  2013: defaultExecutor,
+  2002: defaultExecutor,
+  2008: defaultExecutor,
+};
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,13 @@ import {
 import { logger } from "./logger";
 import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
 import type { SubmittableExtrinsic } from "@polkadot/api/submittable/types";
+import { WHITELIST } from "./executor";
 import dotenv from "dotenv";
 dotenv.config();
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const { PROXIED_ACCOUNT, PROXY_ACCOUNT_SEED, RELAY_ENDPINT } = process.env;
-
-const WHITELIST = [2006, 2012, 2013, 2002, 2008];
 
 async function waitSubqueryIndexBlock(height: number) {
   while (true) {
@@ -73,7 +72,7 @@ async function main() {
     logger.info(`Funds are ${keys}`);
     const availableTasks = await Promise.all(
       keys
-        .filter((k) => WHITELIST.includes(k))
+        .filter((k) => k in WHITELIST)
         .map(async (key) => await fetchContributions(key))
     );
 
@@ -83,21 +82,10 @@ async function main() {
       continue;
     }
 
-    let calls = result.map((t, index) => {
+    const calls = result.map((t, index) => {
       logger.info(`Process tx with ${t.id}`);
-      // batchAll[
-      //  remark(previous_hash)
-      //  proxy(contribute(amount))
-      // ]
-      let txs = [
-        api.tx.system.remark(t.id),
-        api.tx.proxy.proxy(
-          PROXIED_ACCOUNT as string,
-          null,
-          api.tx.crowdloan.contribute(t.paraId, t.amount, null)
-        ),
-      ];
-      return sendTxAndWaitTillFinalized(api.tx.utility.batchAll(txs), index);
+      const txs = WHITELIST[t.paraId](t);
+      return sendTxAndWaitTillFinalized(txs, index);
     });
 
     const callResults = await Promise.all(calls);
